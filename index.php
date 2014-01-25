6191bde06abfc321ed94acb46a7ae4dba7acbd13
Phil Hoyt <hoytph@gmail.com>
2014-01-24 21:35:15 -0500

    MY FIRST COMMIT OMFG!!!!

---
+++ index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>In the High of the Beholder</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css" media="screen" />
+</head>
+<body>
+	<section id="board">
+		<div id="message">
+			<span class="name">Eugene</span><p>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vel ante tempor, tempus orci non, egestas arcu. Nunc ac mauris at tellus pharetra tempus at vitae elit. Sed luctus justo vitae magna placerat bibendum. Proin sit amet pretium leo, a sagittis nunc. Vivamus tempor egestas massa, tincidunt aliquet libero vulputate ut. Phasellus gravida, eros nec suscipit bibendum, leo diam pretium metus, vitae porta tortor nisi a neque. Donec commodo arcu quis est eleifend sollicitudin. Ut convallis quam sit amet nisl fringilla iaculis. Nunc ullamcorper leo non molestie ultricies. Donec laoreet velit eu justo blandit, ac tempor felis vulputate. Phasellus dapibus ullamcorper neque, id aliquet leo fringilla vitae. Phasellus sagittis sodales urna, ut posuere augue molestie ac. Quisque ut consectetur diam, in adipiscing lacus.
+			</p>
+		</div>
+	</section>
+</body>
+</html>
