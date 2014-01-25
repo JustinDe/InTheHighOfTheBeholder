0b6201e8bb6cbeb11ec0c83e04917398d2517547
Phil Hoyt <hoytph@gmail.com>
2014-01-24 23:21:37 -0500

    junk

--- index.php
+++ index.php
@@ -5,6 +5,10 @@
 	<title>In the High of the Beholder</title>
 	<link rel="stylesheet" type="text/css" href="css/style.css" media="screen" />
     <link rel="stylesheet" href="css/animate.css">
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+<!--[if lt IE 9]>
+  <script type="text/javascript" src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
 </head>
 <body>
 
@@ -18,13 +22,14 @@
 </style>
 
 	<section id="board">
-	<div class="char animated fadeInRight"><img src="images/temp.png"></div>
+	<div class="char"><img src="images/temp.png"></div>
 		<div id="message">
 			<span class="name">Eugene</span><p>
 				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vel ante tempor, tempus orci non, egestas arcu. Nunc ac mauris at tellus pharetra tempus at vitae elit. Sed luctus justo vitae magna placerat bibendum. Proin sit amet pretium leo, a sagittis nunc. Vivamus tempor egestas massa, tincidunt aliquet libero vulputate ut. Phasellus gravida, eros nec suscipit bibendum, leo diam pretium metus, vitae porta tortor nisi a neque. Donec commodo arcu quis est eleifend sollicitudin. Ut convallis quam sit amet nisl fringilla iaculis. Nunc ullamcorper leo non molestie ultricies. Donec laoreet velit eu justo blandit, ac tempor felis vulputate. Phasellus dapibus ullamcorper neque, id aliquet leo fringilla vitae. Phasellus sagittis sodales urna, ut posuere augue molestie ac. Quisque ut consectetur diam, in adipiscing lacus.
 			</p>
-			<a href="#" class="next">Next</a>
+			<a href="javascript:void(0)" onclick="$('.char').addClass('animated bounceOutLeft');" class="next">Next</a>
 		</div>
 	</section>
+
 </body>
 </html>
