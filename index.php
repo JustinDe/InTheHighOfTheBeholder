5bed7a626c6c24af99c8e139fc7be9514b2912bd
Phil Hoyt <hoytph@gmail.com>
2014-01-24 22:11:18 -0500

    farts

--- index.php
+++ index.php
@@ -6,11 +6,22 @@
 	<link rel="stylesheet" type="text/css" href="css/style.css" media="screen" />
 </head>
 <body>
+
+<style media="screen" type="text/css">
+
+#board{
+	background: url('') no-repeat top center;
+	background-size: cover;
+}
+
+</style>
+
 	<section id="board">
 		<div id="message">
 			<span class="name">Eugene</span><p>
 				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vel ante tempor, tempus orci non, egestas arcu. Nunc ac mauris at tellus pharetra tempus at vitae elit. Sed luctus justo vitae magna placerat bibendum. Proin sit amet pretium leo, a sagittis nunc. Vivamus tempor egestas massa, tincidunt aliquet libero vulputate ut. Phasellus gravida, eros nec suscipit bibendum, leo diam pretium metus, vitae porta tortor nisi a neque. Donec commodo arcu quis est eleifend sollicitudin. Ut convallis quam sit amet nisl fringilla iaculis. Nunc ullamcorper leo non molestie ultricies. Donec laoreet velit eu justo blandit, ac tempor felis vulputate. Phasellus dapibus ullamcorper neque, id aliquet leo fringilla vitae. Phasellus sagittis sodales urna, ut posuere augue molestie ac. Quisque ut consectetur diam, in adipiscing lacus.
 			</p>
+			<a href="#" class="next">Next</a>
 		</div>
 	</section>
 </body>
