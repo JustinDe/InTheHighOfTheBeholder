f3464a92fd09f17099d92e285730f9ba36f5efa0
Phil Hoyt <hoytph@gmail.com>
2014-01-24 22:49:33 -0500

    Base animation junk

--- index.php
+++ index.php
@@ -4,19 +4,21 @@
 	<meta charset="UTF-8">
 	<title>In the High of the Beholder</title>
 	<link rel="stylesheet" type="text/css" href="css/style.css" media="screen" />
+    <link rel="stylesheet" href="css/animate.css">
 </head>
 <body>
 
 <style media="screen" type="text/css">
 
 #board{
-	background: url('') no-repeat top center;
+	background: url('images/tempbg.jpg') no-repeat top center;
 	background-size: cover;
 }
 
 </style>
 
 	<section id="board">
+	<div class="char animated fadeInRight"><img src="images/temp.png"></div>
 		<div id="message">
 			<span class="name">Eugene</span><p>
 				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vel ante tempor, tempus orci non, egestas arcu. Nunc ac mauris at tellus pharetra tempus at vitae elit. Sed luctus justo vitae magna placerat bibendum. Proin sit amet pretium leo, a sagittis nunc. Vivamus tempor egestas massa, tincidunt aliquet libero vulputate ut. Phasellus gravida, eros nec suscipit bibendum, leo diam pretium metus, vitae porta tortor nisi a neque. Donec commodo arcu quis est eleifend sollicitudin. Ut convallis quam sit amet nisl fringilla iaculis. Nunc ullamcorper leo non molestie ultricies. Donec laoreet velit eu justo blandit, ac tempor felis vulputate. Phasellus dapibus ullamcorper neque, id aliquet leo fringilla vitae. Phasellus sagittis sodales urna, ut posuere augue molestie ac. Quisque ut consectetur diam, in adipiscing lacus.
